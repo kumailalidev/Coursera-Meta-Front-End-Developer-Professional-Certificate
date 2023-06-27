ee68dee4b3e558d13adcbcff8a54c1d2597c049b
Kumail Ali Shaikh <kumailalidev@gmail.com>
2023-06-28 01:07:33 +0500

    Course-02-M01-Added assignments, labs, and exercises.

---
+++ 02-Programming with JavaScript/Module-01-Introduction to JavaScript/M01-S03-10-Exercise-Repetitive tasks with loops.js
@@ -0,0 +1,34 @@
+// Task 1:
+for (var i = 1; i <= 5; i++ ) {
+    console.log(i);
+};
+console.log('Counting completed!');
+
+// Task 2:
+for (var i = 5; i > 0; i--) {
+    console.log(i);
+};
+console.log('Counting completed!');
+
+// Task 3:
+var i = 1;
+while (i < 6) {
+    console.log(i)
+    i++
+};
+console.log('Counting completed');
+
+// Task 4:
+var i = 5;
+while (i > 0) {
+    console.log(i)
+    i = i - 1;
+};
+console.log('Counting completed');
+
+// Task 5:
+var year = 2018;
+while (year <= 2022) {
+    console.log(year)
+    year++
+};
